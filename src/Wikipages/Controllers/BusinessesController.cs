817f1ab64f2fc2e7042f66aff319814fee82d2ae
Anne Belka and Levi Bibo <anne+levi@>
2017-02-06 16:46:49 -0800

    add initial files and directories.

---
+++ src/Wikipages/Controllers/BusinessesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Wikipages.Models;
+
+namespace Wikipages.Controllers
+{
+    public class BusinessesController : Controller
+    {
+        private WikipagesContext db = new WikipagesContext();
+        public IActionResult Index()
+        {
+            return View(db.Businesses.OrderByDescending(business => business.TypeId).ToList());
+        }
+
+        public IActionResult Details(int id)
+        {
+            Business foundBusiness = db.Businesses.FirstOrDefault(businesses => businesses.BusinessId == id);
+            return View(foundBusiness);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Business business)
+        {
+            db.Businesses.Add(business);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ src/Wikipages/Models/Business.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Wikipages.Models
+{
+    [Table("Businesses")]
+    public class Business
+    {
+        [Key]
+        public int BusinessId { get; set; }
+        public string Name { get; set; }
+        public int TypeId { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string WebsiteUrl { get; set; }
+    }
+}
